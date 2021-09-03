6690d5a2446cb0ca5fa8aa1ef87188772a6c81a3
Athanasios Iliopoulos <thanili@hotmail.com>
2021-09-03 17:26:45 +0300

    client app added api adapted End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,8 @@ namespace API
             });
 
             services.AddControllers();
+            services.AddCors();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -51,7 +53,9 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
