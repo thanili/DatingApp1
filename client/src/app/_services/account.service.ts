baab3d1176a4f3c6ec8d5b261525ed6fd39d7da1
Athanasios Iliopoulos <thanili@hotmail.com>
2021-09-14 22:29:29 +0300

    End of section 5

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { ReplaceSource } from 'webpack-sources';
+import { User } from '../models/user';
+import { ReplaySubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService { // An Angular Service is a singleton
+
+  baseUrl = 'https://localhost:44331/api/';
+  private currentUserSource = new ReplaySubject<User>(1);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  setCurrentUser(user: User) {
+    this.currentUserSource.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
+
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'account/register', model).pipe(
+      map((user : User) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+        //return user;
+      })
+    )
+  }
+
+}
---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  model: any = {}
+  //loggedIn: boolean = false;
+  //currentUser$: Observable<User>;
+
+  constructor(public accountService: AccountService) { }
+
+  ngOnInit(): void {
+    //this.getCurrentUser();
+    //this.currentUser$ = this.accountService.currentUser$;
+  }
+
+  login() {
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);
+      //this.loggedIn = true;
+    }, error => {
+        console.log(error);
+    })
+  }
+
+  logout() {
+    this.accountService.logout();
+    //this.loggedIn = false;
+  }
+
+  //getCurrentUser() {
+  //  this.accountService.currentUser$.subscribe(user => {
+  //    this.loggedIn = !!user;
+  //  }, error => {
+  //      console.log(error);      
+  //  })
+  //}
+
+}
