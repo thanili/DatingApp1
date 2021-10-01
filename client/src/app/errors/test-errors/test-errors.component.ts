12caf27aa85d08c8f06174df6e433131686f821c
Athanasios Iliopoulos <thanili@hotmail.com>
2021-10-02 00:41:45 +0300

    End of section 7

---
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -0,0 +1,62 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-test-errors',
+  templateUrl: './test-errors.component.html',
+  styleUrls: ['./test-errors.component.css']
+})
+export class TestErrorsComponent implements OnInit {
+
+  baseUrl = 'https://localhost:44331/api/';
+  validationErrors: string[] = [];
+
+  constructor(private http: HttpClient, private toastr: ToastrService) { }
+
+  ngOnInit(): void {
+  }
+
+  get404Error() {
+    this.http.get(this.baseUrl + 'buggy/not-found').subscribe(response => {
+      console.log(response);
+    }, error => {
+        console.log(error);
+    });
+  }
+
+  get400Error() {
+    this.http.get(this.baseUrl + 'buggy/bad-request').subscribe(response => {
+      console.log(response);
+    }, error => {
+        console.log(error);
+    });
+  }
+
+  get500Error() {
+    this.http.get(this.baseUrl + 'buggy/server-error').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  get401Error() {
+    this.http.get(this.baseUrl + 'buggy/auth').subscribe(response => {
+      console.log(response);
+    }, error => {
+      //this.toastr.error(error.statusText === "OK" ? "Unauthorized" : error.statusText,error.status);
+      console.log(error);
+    });
+  }
+
+  get400ValidationError() {
+    this.http.post(this.baseUrl + 'account/register', {}).subscribe(response => {
+      console.log(response);
+    }, error => {
+        console.log(error);
+        this.validationErrors = error;
+    });
+  }
+
+}
